7e6d10ab1366ce9346d7b8f93b166134938446ec
jjoundi <jamil.joundi@gmail.com>
2024-10-21 16:45:30 +0200

    Update High_Five_Interface.ino

--- High_Five_Interface.ino
+++ High_Five_Interface.ino
@@ -1,9 +1,14 @@
 void setup() {
   // put your setup code here, to run once:
+  // instantiate pushbuttons
+  // instantiate pins for LCD screen
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // If button 1 is pressed increase Bike Counter with 1
+  // If button 2 is pressed increase Pedestrian Counter with 1
+  // If button 3 is pressed increase Car Counter with 1
 
 }
