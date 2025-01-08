ae73f0dee68430e0c4acf2d0cb0c4601c9e3ccf0
Bas Baccarne <bastiaan.baccarne@ugent.be>
2025-01-08 15:32:42 +0100

    hi5 ready

--- tests/MQTT pull_and_write.py
+++ tests/MQTT pull_and_write.py
@@ -5,9 +5,10 @@
 
 #libraries
 import paho.mqtt.client as mqtt  
-
 import gspread
+import json
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime 
 
 # MQTT broker details
 BROKER = "broker.emqx.io"
@@ -22,7 +23,9 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("./tests/key.json", sco
 # Authorize the client
 client = gspread.authorize(creds)
 # Open the Google Sheet using the URL
-sheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1YUWfkA1w6GezTzYUTnjYmWHWe1nIyDaAK0Ytp2pUTw0/edit').sheet1
+spreadsheet = client.open_by_url('https://docs.google.com/spreadsheets/d/1YUWfkA1w6GezTzYUTnjYmWHWe1nIyDaAK0Ytp2pUTw0/edit')
+sheet_live = spreadsheet.worksheet('Hi5_live') # name of the sheet with live data
+sheet_log = spreadsheet.worksheet('Hi5_log') # name of the sheet with log data
 
 # Callback when the client connects to the broker
 def on_connect(client, userdata, flags, rc):
@@ -37,15 +40,40 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(f"Received message: {msg.payload.decode()} on topic: {msg.topic}")
 
-    # data = msg.payload.decode()
-    data = [
-    ["Name", "Age", "City"],
-    ["Alice", 30, "New York"],
-    ["Bob", 25, "Los Angeles"],
-    ["Charlie", 35, "Chicago"]
-]
-    for row in data:
-        sheet.append_row(row)
+    # Decode the message
+    data = msg.payload.decode()
+
+    # Load the JSON data into a dictionary
+    data_dict = json.loads(data)
+    
+    # Transform the dictionary to the desired structure for writing to Google Sheets
+    transformed_data = [["Mode of Transport", "Count"]]
+    for key, value in data_dict.items():
+        transformed_data.append([key, value])
+    
+    # Print the transformed data (debgugging)
+    # print(transformed_data)
+
+    # Clear the existing content in the sheet
+    sheet_live.clear()
+
+    # Append the transformed data to the Google sheet (live data sheet)
+    for row in transformed_data:
+        sheet_live.append_row(row)
+
+    # Get the current timestamp for log data
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    # Append the data to the log sheet
+    anno_data = [["Mode of Transport", "Count", "Timestamp"]]
+    for key, value in data_dict.items():
+        anno_data.append([key, value, timestamp])
+
+    # Append the data to the Google sheet (log data sheet)
+    for row in anno_data:
+        sheet_log.append_row(row)
+    
+    print("Data written to Google Sheets!")
 
 # Create MQTT client instance
 client = mqtt.Client()
